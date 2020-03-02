721a0faafcc05c901de00232f5bbe2000431d0ce
werb <werbhelius@gmail.com>
2020-03-02 16:07:07 +0800

    bug fix

--- app/src/main/java/com/werb/darkmode/MainActivity.kt
+++ app/src/main/java/com/werb/darkmode/MainActivity.kt
@@ -2,6 +2,7 @@ package com.werb.darkmode
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.observe
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.werb.darkmode.databinding.ActivityMainBinding
@@ -22,6 +23,23 @@ class MainActivity : AppCompatActivity() {
 
     private fun makeUI() {
         binding.toolbar.inflateMenu(R.menu.main_menu)
+        binding.toolbar.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.light -> {
+                    delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
+                }
+                R.id.dark -> {
+                    delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
+                }
+                R.id.system -> {
+                    delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                }
+                R.id.menu_battery_saver -> {
+                    delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+                }
+            }
+            return@setOnMenuItemClickListener true
+        }
         binding.list.layoutManager = manager
         binding.list.addItemDecoration(GridSpacingItemDecoration(2, 16))
         binding.list.adapter = adapter
