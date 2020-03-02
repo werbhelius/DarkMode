41b1a35dc033fca4d6bed79299bb6b4b5af3a889
werb <werbhelius@gmail.com>
2020-03-02 11:56:37 +0800

    md

--- app/src/main/java/com/werb/darkmode/EventViewHolder.kt
+++ app/src/main/java/com/werb/darkmode/EventViewHolder.kt
@@ -3,6 +3,7 @@ package com.werb.darkmode
 import androidx.recyclerview.widget.RecyclerView
 import com.werb.darkmode.databinding.LayoutItemBinding
 import com.werb.darkmode.model.Event
+import com.werb.darkmode.utils.getColorFromAttr
 
 /**
  * Created by wanbo on 2020/2/29.
@@ -11,6 +12,17 @@ class EventViewHolder(private val binding: LayoutItemBinding): RecyclerView.View
 
     fun bind(event: Event) {
         binding.event = event
+        if (event.isError) {
+            binding.cardView.setCardBackgroundColor(binding.root.context.getColorFromAttr(R.attr.colorError))
+            binding.data.setTextColor(binding.root.context.getColorFromAttr(R.attr.colorOnError))
+            binding.desc.setTextColor(binding.root.context.getColorFromAttr(R.attr.colorOnError))
+            binding.title.setTextColor(binding.root.context.getColorFromAttr(R.attr.colorOnError))
+        } else {
+            binding.cardView.setCardBackgroundColor(binding.root.context.getColorFromAttr(R.attr.colorSurface))
+            binding.data.setTextColor(binding.root.context.getColorFromAttr(R.attr.colorOnSurface))
+            binding.desc.setTextColor(binding.root.context.getColorFromAttr(R.attr.colorOnSurface))
+            binding.title.setTextColor(binding.root.context.getColorFromAttr(R.attr.colorOnSurface))
+        }
     }
 
 }
